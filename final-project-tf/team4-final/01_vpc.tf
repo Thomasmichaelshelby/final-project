19e7a8ca0e8252ddbbc32774acc129d62c263dd7
Thomasmichaelshelby <114216018+Thomasmichaelshelby@users.noreply.github.com>
2023-10-06 15:37:55 +0900

    Delete final-project-tf/team4-final directory

--- final-project-tf/team4-final/01_vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "team4_vpc" {
-  cidr_block           = var.cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "${var.tag}-vpc"
-  }
-}
--- final-project-tf/team4-final/04_ig.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "team4_ig" {
-  vpc_id = aws_vpc.team4_vpc.id
-
-  tags = {
-    Name = "${var.tag}-ig"
-  }
-}
